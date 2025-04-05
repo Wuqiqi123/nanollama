bebb9a4b431ebb71a4b7c07e184e068c64d68c87
wqq <1258552199@qq.com>
2025-04-05 17:15:07 +0800

    udpate

--- gpt2.py
+++ gpt2.py
@@ -13,21 +13,40 @@ class GPTConfig:
     n_layer: int = 12
     n_head: int = 12
     n_embd: int = 768
+    use_flash_attn: bool = False
+    dropout: float = 0.0
 
 
-class CausalSelfAttention(torch.Module):
+class CausalSelfAttention(nn.Module):
     def __init__(self, config: GPTConfig):
+        super().__init__()
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+
         assert config.n_embd % config.n_head == 0
-        self.atten = nn.Linear(config.n_embd, 3 * config.n_embd)
-        self.proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+        self.o_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.use_flash_attn = config.use_flash_attn
+        self.dropout = config.dropout
+        self.attn_dropout = nn.Dropout(config.dropout)
+
 
     def forward(self, x):
         B, T, C = x.shape
-        q, k, v = self.atten(x).split(3, dim=-1)
-        q = q * (C ** -0.5)
-        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
-        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
-        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        q, k, v = self.c_attn(x).chunk(3, dim=-1)
+        q = einx.rearrange("b q (h c) -> b h q c", q, h=self.n_head)
+        k = einx.rearrange("b k (h c) -> b h k c", k, h=self.n_head)
+        v = einx.rearrange("b v (h c) -> b h v c", v, h=self.n_head)
+
+        if self.use_flash_attn:
+            y = torch.nn.functional.scaled_dot_product_attention(q, k, v, 
+            attn_mask=None, dropout_p=self.dropout if self.training else 0, is_causal=True)
+        else:
+            einx.dot("b h q [c], b h k [c] -> b h q k", q, k) / ( q.shape[-1] ** 0.5)
+
+
+        
+
 
 
     # def __call__(self, x):
@@ -67,3 +86,11 @@ class CausalSelfAttention(torch.Module):
     #     x = x + x0
 
     #     return x
+
+
+if __name__ == "__main__":
+    config = GPTConfig()
+    attention = CausalSelfAttention(config)
+    x = torch.randn(1, 1024, 768)
+    attention(x)
+
--- llama.py
+++ llama.py
@@ -1,29 +1,2 @@
-import torch
-import torch.nn as nn
-from torch.nn import functional as F
-from dataclasses import dataclass
-
-@dataclass
-class GPTConfig:
-    block_size: int = 1024
-    vocab_size: int = -1
-    n_layer: int = 12
-    n_head: int = 12
-    n_embd: int = 768
-
-class CausalSelfAttention(nn.Module):
-    def __init__(self, config):
-        assert config.n_embd % config.n_head == 0
-        super().__init__()
-        self.n_head = config.n_head
-        self.n_embd = config.n_embd
-        self.n_block_size = config.block_size
-        self.n_ctx = config.block_size
-        self.wq = nn.Parameter(torch.randn((self.n_embd, self.n_embd)))
-        self.wk = nn.Parameter(torch.randn((self.n_embd, self.n_embd)))
-        self.wv = nn.Parameter(torch.randn((self.n_embd, self.n_embd)))
-        self.wo = nn.Parameter(torch.randn((self.n_embd, self.n_embd)))
-        
-
 
 
